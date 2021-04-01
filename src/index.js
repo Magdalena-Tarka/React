e05bd8850a9dda43c42b5795233fb00a12664270
Magdalena Tarka <mxtarka@gmail.com>
2021-04-01 19:24:13 +0200

    Add components List and Hero

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
+// Znajdź element, którego id to 'app', i wstaw do niego komponent App

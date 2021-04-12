d1598f9a3d654a3bb955a023cab69a175cd30624
Magdalena Tarka <mxtarka@gmail.com>
2021-04-12 02:25:59 +0200

    Implement redux in adding new column and new card

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,6 +4,7 @@ import Hero from '../Hero/Hero';
 import Column from '../Column/ColumnContainer';
 import styles from './List.scss';
 import PropTypes from 'prop-types';
+import Creator from '../Creator/Creator';
 
 class List extends React.Component {
   static propTypes = {
@@ -11,6 +12,7 @@ class List extends React.Component {
     image: PropTypes.string,
     description: PropTypes.node,
     columns: PropTypes.array,
+    addColumn: PropTypes.func,
   }
 
   static defaultProps = {
@@ -18,7 +20,7 @@ class List extends React.Component {
   }
 
   render() {
-    const {title, image, description, columns} = this.props;
+    const {title, image, description, columns, addColumn} = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} imageText={image} />
@@ -30,9 +32,9 @@ class List extends React.Component {
             <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        {/*<div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
-          </div>*/}
+        <div className={styles.creator}>
+          <Creator text={settings.columnCreatorText} action={addColumn}/>
+        </div>
       </section>
     );
   }
